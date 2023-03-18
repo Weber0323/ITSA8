5c2cd577150fe9c200484924b900b1a597314b4a
瑋伯 翁 <4b1g0135@office.stust.edu.tw>
2023-03-19 01:12:13 +0800

    加入專案檔案。

---
+++ ITSA8/ITSA8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>  
+ 
+using namespace std;
+
+int main() {
+    int n, i;
+    while (cin >> n)
+    {
+
+
+        for (i = 2; i < n; i++) {
+            if (n % i == 0) {
+                break;
+            }
+        }
+        if (i == n) {
+            cout << "yes" << endl;
+        }
+        else {
+            cout << "no" << endl;
+        }
+    }
+}
